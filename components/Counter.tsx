d38d87ed1d0aaf4342fa65423f4f1998b9277ff1
Dev Lukale <devluka99@gmail.com>
2024-07-12 19:47:21 +0300

    Changes to be committed: 	modified:   components/Counter.tsx

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -20,6 +20,7 @@ export function Counter({ name }: { name: string }) {
     <div className="flex items-center gap-x-4">
       <input type="hidden" name={name} value={amount} />
       <Button variant="outline" size="icon" type="button" onClick={decrease}>
+      
         <Minus className="h-4 w-4 text-primary" />
       </Button>
       <p className="font-medium text-lg">{amount}</p>
